caeca39d0b1ea9ef86542ec4e9a72f2ae6f93f1c
Jo Pi <ferrdo@gmail.com>
2018-09-04 17:09:49 +0200

    Update led_bars.ino
    support for 10 to 8

--- KerbalController/led_bars.ino
+++ KerbalController/led_bars.ino
@@ -34,3 +34,23 @@ void setNum(int num, int disp)
     leds[j] = (1<<k);
 }
 
+void setValues(byte v1, byte v2, byte v3, byte v4, byte v5)
+{
+  for (int i = 0; i < 7; ++i)
+  {
+    Data[i] = 0;
+  }
+  int pv1 = 0.1+pow(2, v1);
+  int pv2 = 0.1+pow(2, v2);
+  int pv3 = 0.1+pow(2, v3);
+  int pv4 = 0.1+pow(2, v4);
+  int pv5 = 0.1+pow(2, v5);
+
+  Data[0] = pv5 >> 8;
+  Data[1] = pv5;
+  Data[2] = pv4 >> 2;
+  Data[3] = (pv4 << 6) | (pv3 >> 4);
+  Data[4] = (pv3 << 4) | (pv2 >> 6);
+  Data[5] = (pv2 << 2) | (pv1 >> 8);
+  Data[6] = pv1;
+}
